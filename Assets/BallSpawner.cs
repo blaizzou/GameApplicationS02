643c3f2ce46cff673dfb55a8ac2df51957bf803d
Alexandre <alexandre.gueguen@epitech.eu>
2020-05-13 15:17:12 +0900

    first push, basic vr controls + bouncing ball

---
+++ Assets/BallSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ball;
+    private float timer;
+
+    void Start() {
+        // Instantiate(ball, transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+            timer = 2f;
+        }
+    }
+}
