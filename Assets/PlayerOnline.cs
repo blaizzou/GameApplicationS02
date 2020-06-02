60f72175bc6f319330daf5aee14f1bfc7918a710
Alexandre <alexandre.gueguen@epitech.eu>
2020-06-02 19:09:40 +0900

    multi sync movement

---
+++ Assets/PlayerOnline.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOnline : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject playerVR;
+    void Start()
+    {
+        playerVR.SetActive(true);
+    }
+}
