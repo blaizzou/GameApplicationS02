b0f3edd6722c2864f529575f80dd64c3c8471a41
blaise <blaise.carnevillier@epitech.eu>
2020-06-16 16:42:07 +0900

    test input

--- Assets/Script/ValveInput.cs
+++ Assets/Script/ValveInput.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using Valve.VR;
 using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
 
-public class ValveInput : MonoBehaviour
-{
+public class ValveInput : MonoBehaviour {
+    public CanvasGroup uiElement;
+    private bool UI = false;
+    private int tmp = 0;
     private GameObject ball;
     private GameObject Lefthand;
 
@@ -13,32 +16,70 @@ public class ValveInput : MonoBehaviour
     private int counter;
     private bool hold = false;
     private PhotonView PV;
+    private void Start () {
 
-    private void Start()
-    {
-        if (!PV.IsMine && PhotonNetwork.IsConnected)
-            this.enabled = false;
-        ball = GameObject.FindGameObjectWithTag("projectile");
-        Lefthand = GameObject.FindGameObjectWithTag("LeftHand");
+        // if (!PV.IsMine && PhotonNetwork.IsConnected)
+        //     this.enabled = false;
+        FadeOut();
+        ball = GameObject.FindGameObjectWithTag ("projectile");
+        Lefthand = GameObject.FindGameObjectWithTag ("LeftHand");
     }
-    void Update()
-    {
-
+    void Update () {
+        tmp++;
+        Debug.Log(tmp);
+        if ( tmp >= 500) {
+            tmp = 0;
+            if (UI)
+                FadeIn();
+            else
+                FadeOut();
+            UI = !UI;
+        }
         if (SteamVR_Actions.default_DropBall.changed && hold)
             hold = false;
         else if (SteamVR_Actions.default_DropBall.changed)
             hold = true;
         if ((SteamVR_Actions.default_DropBall.changed || hold))
-            AppearBall();
+            AppearBall ();
+        if (SteamVR_Actions.default_fade_UI.changed)
 
-        counter++;
-        
+            counter++;
+
     }
 
-    void AppearBall()
-    {
+    void AppearBall () {
         ball.transform.position = Lefthand.transform.position;
-        ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        ball.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
         counter = 0;
     }
+
+    public void FadeIn () {
+        StartCoroutine (FadeCanvasGroup (uiElement, uiElement.alpha, 1, .5f));
+    }
+
+    public void FadeOut () {
+        StartCoroutine (FadeCanvasGroup (uiElement, uiElement.alpha, 0, .5f));
+    }
+
+    public IEnumerator FadeCanvasGroup (CanvasGroup cg, float start, float end, float lerpTime = 1) {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true) {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp (start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForFixedUpdate ();
+        }
+
+        print ("done");
+    }
+
 }
