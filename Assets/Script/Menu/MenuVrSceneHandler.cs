e62fca1d2bcd5f4d3c86959aa4f59f086114fd09
Arthjeed <arthjeed@hotmail.fr>
2020-06-19 20:29:56 +0900

    New pointer script

---
+++ Assets/Script/Menu/MenuVrSceneHandler.cs
@@ -0,0 +1,63 @@
+﻿/* SceneHandler.cs*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+
+public class MenuVrSceneHandler : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "training btn")
+        {
+            Debug.Log("training btn was clicked");
+        } else if (e.target.name == "multiplayer btn")
+        {
+            Debug.Log("multiplayer btn was clicked");
+        }
+        else if (e.target.name == "exit btn")
+        {
+            Debug.Log("exit btn was clicked");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+        }
+        
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        /*if (e.target.name == "training btn")
+        {
+            Debug.Log("training btn was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }*/
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        /*if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }*/
+    }
+}
