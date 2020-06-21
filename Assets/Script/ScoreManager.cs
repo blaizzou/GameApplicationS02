9148409c40d2511e2e578bcc3b0d36bdf55d754e
isocele <hugo.chollet@epitech.eu>
2020-06-21 20:19:29 +0900

    basic Chrono

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -6,15 +6,25 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     private Text score;
+    public float chrono;
     public static int scoreValue = 0;
+
+    private float time;
     void Start()
     {
+        time = chrono;
         score = GetComponent<Text>();   
     }
 
     
     void Update()
     {
-        score.text = "Point : " + scoreValue;
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+            score.text = "Point : " + scoreValue + "\nRemaining Time : " + (int)time;
+
+        }
+
     }
 }
