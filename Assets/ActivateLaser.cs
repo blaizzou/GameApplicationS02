c74058a23999ab3336c8e3f5fb39e49797f46061
blaise <blaise.carnevillier@epitech.eu>
2020-06-21 23:36:57 +0900

    laser

--- Assets/ActivateLaser.cs
+++ Assets/ActivateLaser.cs
@@ -7,14 +7,13 @@ public class ActivateLaser : MonoBehaviour {
     public SteamVR_LaserPointer pointer;
     public GameObject Goal1;
     public GameObject Goal2;
-
     private Goal g1;
     private Goal g2;
     // Start is called before the first frame update
     void Start () {
         pointer = GetComponent<SteamVR_LaserPointer> ();
-        Goal1 =  GameObject.Find("/Goal1");
-        Goal2 =  GameObject.Find("/Goal2");
+        Goal1 = GameObject.Find("/Goal1");
+        Goal2 = GameObject.Find("/Goal2");
         g1 = Goal1.GetComponent<Goal> ();
         g2 = Goal2.GetComponent<Goal> ();
     }
