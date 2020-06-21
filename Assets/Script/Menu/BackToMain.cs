6917b9293964562f63df9b0447265b0131f5ff34
Arthjeed <arthjeed@hotmail.fr>
2020-06-21 20:53:39 +0900

    MenuVr

---
+++ Assets/Script/Menu/BackToMain.cs
@@ -0,0 +1,45 @@
+﻿/* SceneHandler.cs*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+using UnityEngine.SceneManagement;
+
+public class BackToMain : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+    private Vector3 scaleChange = new Vector3(0.1f, 0.1f, 0f);
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "backToMainBtn")
+        {
+            SceneManager.LoadScene ("MenuVr");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+       if (e.target.name == "backToMainBtn")
+        {
+            e.target.localScale += scaleChange;
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "backToMainBtn")
+        {
+            e.target.localScale -= scaleChange;
+        }
+    }
+}
